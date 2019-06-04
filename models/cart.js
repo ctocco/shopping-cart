e7853d68bf29dddabcecc06a40832b8ea02dd9c2
Claudio Tocco <tocco.a.claudio@gmail.com>
2019-06-04 10:11:39 +0200

    adding getter methods on classes

--- models/cart.js
+++ models/cart.js
@@ -1,27 +1,36 @@
 const cart = {
   "5cf502eddbe9a6104529cc88": {
-    productDetails: {
-      _id: "5cf502eddbe9a6104529cc88",
-      name: "Chips",
-      description: "Commodi ipsa officia qui numquam quibusdam excepturi.",
-      imageurl: "http://lorempixel.com/640/480/abstract",
-      price: 909
-    }
+    _id: "5cf502eddbe9a6104529cc88",
+    name: "Chips",
+    description: "Commodi ipsa officia qui numquam quibusdam excepturi.",
+    imageurl: "http://lorempixel.com/640/480/abstract",
+    price: 909
   },
   "5cf502eddbsdfasdf29cc88": {
-    productDetails: {
-      _id: "5cf502eddbsdfasdf29cc88",
-      name: "Chips",
-      description: "Commodi ipsa officia qui numquam quibusdam excepturi.",
-      imageurl: "http://lorempixel.com/640/480/abstract",
-      price: 909
-    }
+    _id: "5cf502eddbsdfasdf29cc88",
+    name: "Chocolate",
+    description: "Commodi ipsa officia qui numquam quibusdam excepturi.",
+    imageurl: "http://lorempixel.com/640/480/abstract",
+    price: 456
   }
 };
-
 class Cart {
   constructor() {
     this.items = {};
+  }
+  get totalQty() {
+    let qty = 0;
+    for (let item in this.items) {
+      qty += this.items[item].qty;
+    }
+    return qty;
+  }
+  get totalPrice() {
+    let price = 0;
+    for (let item in this.items) {
+      price += this.items[item].price;
+    }
+    return price;
   }
 
   add(product) {
@@ -37,7 +46,6 @@ class Cart {
       };
     }
   }
-
   remove(product) {
     const item = this.items[product._id];
     if (item.qty > 1) {
@@ -45,11 +53,16 @@ class Cart {
       item.price = item.qty * product.price;
     }
   }
-
   removeAll() {
     this.items = {};
   }
 }
+const shoppingCart = new Cart();
 
-let updateCart = new Cart();
-updateCart.add(cart["5cf502eddbe9a6104529cc88"]
+shoppingCart.add(cart["5cf502eddbe9a6104529cc88"]);
+shoppingCart.add(cart["5cf502eddbsdfasdf29cc88"]);
+shoppingCart.add(cart["5cf502eddbsdfasdf29cc88"]);
+shoppingCart.add(cart["5cf502eddbsdfasdf29cc88"]);
+
+console.log(shoppingCart.totalQty);
+console.log(shoppingCart.totalPrice);
