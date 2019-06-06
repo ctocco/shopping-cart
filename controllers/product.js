2f1a816c6c403296f6c10e3693d89bdf8b5787bb
Claudio Tocco <tocco.a.claudio@gmail.com>
2019-06-06 18:06:00 +0200

    adding product by id method

--- controllers/product.js
+++ controllers/product.js
@@ -5,7 +5,6 @@ var app = express();
 
 module.exports = {
   getAllProducts: async (req, res, next) => {
-    console.log(req.session);
     try {
       const productList = await Product.find();
       res.json(productList);
@@ -13,7 +12,13 @@ module.exports = {
       error.message = noProducts;
       next(error);
     }
+  },
+  getProductById: async (req, res, next) => {
+    try {
+      const product = await Product.find(_id);
+      res.json(product);
+    } catch (error) {
+      error.message = noProducts;
+    }
   }
 };
-
-
--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ const { getAllProducts } = require("../controllers/product");
 
 const apiRoutes = {
   getAllProducts: "/products",
+  getProductById: "/products/:id",
   addProduct: "/add/:id",
   removeProduct: "/remove/:id",
   removeAllProduct: "remove-all/:id"
@@ -16,5 +17,6 @@ apiRouter.get("/", (req, res) => {
 });
 // apiRouter.use(sendJsonResp);
 apiRouter.get(apiRoutes.getAllProducts, getAllProducts);
+apiRouter.get(apiRoutes.getProductById, getAllProducts);
 
 module.exports = apiRouter;
